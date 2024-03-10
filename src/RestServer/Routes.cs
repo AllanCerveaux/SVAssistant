6eac0e4a172c73d030eb82fbd3ea3a013d8ed5b9
Allan <allan.cerveaux@solotech.com>
2024-03-10 11:03:38 +0100

    Add Cors, Rate limit, and route Error

--- src/RestServer/Routes.cs
+++ src/RestServer/Routes.cs
@@ -12,6 +12,8 @@ namespace Rest
 
 	internal class Routes
 	{
+		private static HttpListenerResponse Response;
+		private static HttpListenerRequest Request;
 		private readonly List<Route> routes = new List<Route>();
 
 		private void Add(Route route)
@@ -31,24 +33,70 @@ namespace Rest
 
 		public void HandleResquest(HttpListenerContext context)
 		{
-			var request = context.Request;
-			var response = new RouteHttpResponse(context.Response);
-			var path = request.Url.AbsolutePath;
-			var method = request.HttpMethod;
+			Response = context.Response;
+			Request = context.Request;
+
+			HeaderConfiguration();
+
+			var method = Request.HttpMethod;
+			var path = Request.Url.AbsolutePath;
+
+			RouteHttpResponse routeHttpResponse = new RouteHttpResponse(Response);
 
 			var route = routes.FirstOrDefault(route =>
 				route.Path == path && route.Method.ToString() == method
 			);
 
-			if (route != null)
+			if (route is null)
 			{
-				route.Action(request, response, context);
+				routeHttpResponse.Error(HttpStatusCode.NotFound, "Route Not Found");
 				return;
 			}
 
-			context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-			context.Response.Close();
+			if(!isRateLimitExeeded(Request.RemoteEndPoint.Address.ToString()))
+			{
+				routeHttpResponse.Error(HttpStatusCode.TooManyRequests, "Too Many Resquests");
+				return;
+			}
+			
+			route.Action(Request, routeHttpResponse, context);
 		}
+
+		private static void HeaderConfiguration(string CorsOrigin = "*", string CorsMethods = "GET, POST")
+		{
+			Response.Headers.Add("Access-Control-Allow-Origin", CorsOrigin);
+			Response.Headers.Add("Access-Control-Allow-Methods", CorsMethods);
+		}
+
+		private readonly Dictionary<string, RequestInfo> _rateLimitingDic = new Dictionary<string, RequestInfo>();
+		// @TODO: Change this arbitary value.
+		private const int Limit = 100;
+		private readonly TimeSpan ResetPeriod = TimeSpan.FromHours(1);
+
+		private bool isRateLimitExeeded(string ipAddr)
+		{
+			if(_rateLimitingDic.ContainsKey(ipAddr))
+			{
+				_rateLimitingDic[ipAddr] = new RequestInfo {Count = 1, LastRest = DateTime.Now};
+				return false;
+			}
+
+			var info = _rateLimitingDic[ipAddr];
+			if((DateTime.Now - info.LastRest) > ResetPeriod)
+			{
+				info.Count = 1;
+				info.LastRest = DateTime.Now;
+				return false;
+			}
+
+			if(info.Count >= Limit)
+			{
+				return true;
+			}
+
+			info.Count++;
+			return false;
+		} 
 	}
 
 	internal class Route
@@ -84,5 +132,22 @@ namespace Rest
 			_response.OutputStream.Write(buffer, 0, buffer.Length);
 			_response.OutputStream.Close();
 		}
+
+		public void Error(HttpStatusCode code, string message)
+		{
+			_response.ContentType = "application/json";
+			_response.StatusCode = (int)code;
+			var jsonResponse = JsonSerializer.Serialize(new { code, message });
+			var buffer = Encoding.UTF8.GetBytes(jsonResponse);
+			_response.ContentLength64 = buffer.Length;
+			_response.OutputStream.Write(buffer, 0, buffer.Length);
+			_response.OutputStream.Close();
+		}
+	}
+
+	public class RequestInfo
+	{
+		public int Count {get; set;}
+		public DateTime LastRest {get; set;}
 	}
 }
