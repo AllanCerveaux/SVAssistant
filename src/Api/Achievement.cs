67611bfdb2d67f120c2081020a7dfa27b69c2f7f
Allan <allan.cerveaux@solotech.com>
2024-03-14 17:24:04 +0100

    Use Linq instead of loop

--- src/Api/Achievement.cs
+++ src/Api/Achievement.cs
@@ -2,36 +2,49 @@ namespace SVAssistant.Api
 {
 	public class AchievementDTO
 	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public string Description { get; set; }
-		public bool IsUnlocked { get; set; }
+		public int id { get; set; }
+		public string name { get; set; }
+		public string description { get; set; }
+		public bool originalStatus { get; set; }
+		public int nextId { get; set; }
+		public int previousId { get; set; }
+		public bool isUnlocked { get; set; }
 	}
 
 	internal static class Achievements
 	{
+		public static AchievementDTO AchievementsParse(
+			int id,
+			string achievement,
+			Netcode.NetIntList famerAchivements
+		)
+		{
+			var parts = achievement.Split('^');
+			var name = parts[0].Trim();
+			var description = parts[1];
+			var originalStatus = bool.Parse(parts[2]);
+			var previousId = int.Parse(parts[3]);
+			var nextId = int.Parse(parts[4]);
+
+			return new AchievementDTO
+			{
+				id = id,
+				name = name,
+				description = description,
+				originalStatus = originalStatus,
+				previousId = previousId,
+				nextId = nextId,
+				isUnlocked = famerAchivements.Contains(id)
+			};
+		}
+
 		public static List<AchievementDTO> GetPlayerAchievement(StardewValley.Farmer farmer)
 		{
-			List<AchievementDTO> data = new List<AchievementDTO>();
-
-			foreach (var achievement in StardewValley.Game1.achievements)
-			{
-				string name = achievement.Value.Split('^')[0];
-				string description = achievement.Value.Split('^')[1];
-				bool isUnlocked = farmer.achievements.Contains(achievement.Key);
-
-				data.Add(
-					new AchievementDTO
-					{
-						Id = achievement.Key,
-						Name = name,
-						Description = description,
-						IsUnlocked = isUnlocked
-					}
-				);
-			}
-
-			return data;
+			return StardewValley
+				.Game1.achievements.Select(item =>
+					AchievementsParse(item.Key, item.Value, farmer.achievements)
+				)
+				.ToList();
 		}
 	}
 }
